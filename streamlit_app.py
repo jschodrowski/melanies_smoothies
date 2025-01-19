580aabf02ab9e26284bcea2cf49bfdb8e7c8b29e
jschodrowski <jschodrowski@yahoo.com>
2025-01-19 17:12:38 -0500

    Update streamlit_app.py
    Added API information for nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+#new section to bring in nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
