bbf5fd9ac6386dc359a4714e0a7d027d7d15a49e
jschodrowski <jschodrowski@yahoo.com>
2025-01-23 02:03:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,4 +60,4 @@ smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/wa
 #smoothiefroot_response = requests.get("https://www.fruityvice.com/#3")
 #st.text(smoothiefroot_response)
 st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = true)
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
